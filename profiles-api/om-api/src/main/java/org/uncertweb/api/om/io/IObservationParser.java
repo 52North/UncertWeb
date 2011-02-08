d8c63f03a18075e71c639c98f8bdc3f3b6b45ddc
Christoph Stasch <c.stasch@52north.org>
2011-02-08 21:40:04 +0000

    changed to new collection types

--- profiles-api/om-api/src/main/java/org/uncertweb/api/om/io/IObservationParser.java
+++ profiles-api/om-api/src/main/java/org/uncertweb/api/om/io/IObservationParser.java
@@ -2,14 +2,9 @@ package org.uncertweb.api.om.io;
 
 import java.net.URISyntaxException;
 
-import net.opengis.om.x20.FoiPropertyType;
-import net.opengis.om.x20.OMObservationCollectionDocument;
-import net.opengis.om.x20.OMObservationDocument;
-
 import org.apache.xmlbeans.XmlException;
 import org.uncertweb.api.om.observation.AbstractObservation;
-import org.uncertweb.api.om.observation.ObservationCollection;
-import org.uncertweb.api.om.sampling.SpatialSamplingFeature;
+import org.uncertweb.api.om.observation.collections.IObservationCollection;
 
 /**
  * Interface for parsing observations 
@@ -28,7 +23,7 @@ public interface IObservationParser {
 	 * @throws XmlException
 	 * @throws URISyntaxException
 	 */
-	public ObservationCollection parseObservationCollection(String xmlObsCol)
+	public IObservationCollection parseObservationCollection(String xmlObsCol)
 			throws Exception, XmlException, URISyntaxException;
 	
 	/**
@@ -43,25 +38,4 @@ public interface IObservationParser {
 	public AbstractObservation parseObservation(String xmlObs)
 			throws Exception, XmlException, URISyntaxException;
 
-	/**
-	 * parses an Observation and it's SpatialSamplingFeature
-	 * 
-	 * @param xb_obsDoc xml observation document
-	 * @return
-	 * @throws Exception
-	 * @throws XmlException
-	 * @throws URISyntaxException
-	 */
-	public AbstractObservation parseObservationDocument(OMObservationDocument xb_obsDoc)
-			throws Exception, XmlException, URISyntaxException;
-	
-	/**
-	 * parses a SpatialSamlingFeature from a feature of interest
-	 * 
-	 * @param xb_featureOfInterest
-	 * @return
-	 * @throws Exception
-	 */
-	public SpatialSamplingFeature parseSamplingFeature(
-			FoiPropertyType xb_featureOfInterest) throws Exception;
 }
