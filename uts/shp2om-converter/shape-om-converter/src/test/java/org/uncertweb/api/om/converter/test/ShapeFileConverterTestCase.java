80d702783f141761a51f45b16e9c7efd390f81cf
Christoph Stasch <c.stasch@52north.org>
2011-02-11 08:13:40 +0000

    Junit test for converter

---
+++ uts/shp2om-converter/shape-om-converter/src/test/java/org/uncertweb/api/om/converter/test/ShapeFileConverterTestCase.java
@@ -0,0 +1,28 @@
+package org.uncertweb.api.om.converter.test;
+
+import java.io.IOException;
+
+import org.uncertweb.api.om.converter.ShapeFileConverter;
+
+import junit.framework.TestCase;
+
+public class ShapeFileConverterTestCase extends TestCase {
+	
+	private static final String FILE_PATH="file://D:/IfGI/Projekte/UncertWeb/Implementations/uw_workspace/shape-om-converter/src/main/resources";
+	
+	public void setUp() {
+	}
+	
+//	public void testConverter() throws Exception{
+//		ShapeFileConverter converter = new ShapeFileConverter();
+//		converter.convertShp2OM(FILE_PATH+"/shp/lqMS.shp", "");
+//	}
+	
+	public void testUncertaintyConverter() throws Exception{
+		ShapeFileConverter converter = new ShapeFileConverter();
+		converter.convertSHPnDBF2OM(FILE_PATH+"/shapefiles/Traffic_Muenster.shp", FILE_PATH.replace("file://","")+"/shapefiles/Traffic_uncertainties.dbf", "");
+	}
+	
+	public void tearDown() {
+	}
+}
