3ff4864c7c2e2d49bc20512ce553cc60efa5467f
Christoph Stasch <c.stasch@52north.org>
2011-02-11 08:10:50 +0000

    Util class contains some useful method which might also be used outside of converter

---
+++ uts/shp2om-converter/shape-om-converter/src/main/java/org/uncertweb/api/om/converter/ShapeFileConverterUtil.java
@@ -0,0 +1,35 @@
+package org.uncertweb.api.om.converter;
+
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.uncertweb.api.om.TimeObject;
+import org.uncertweb.api.om.io.XBObservationParser;
+
+public class ShapeFileConverterUtil {
+	
+	public static  TimeObject parsePhenTime(String phenTime) throws Exception{
+		if (phenTime!=null&&!phenTime.equals("")){
+			XBObservationParser xbParser = new XBObservationParser();
+			TimeObject to = null;
+			String[] times = phenTime.split(",");
+			if (times.length==1){
+				DateTime timePosition = xbParser.parseTimePosition(times[0]);
+				to = new TimeObject(null,timePosition);
+				return to;
+			}
+			else if (times.length==2){
+				DateTime beginPosition = xbParser.parseTimePosition(times[0]);
+				DateTime endPosition = xbParser.parseTimePosition(times[1]);
+				to = new TimeObject(null,new Interval(beginPosition,endPosition));
+				return to;
+			}
+			else {
+				throw new Exception("Phenomenon Time can only be time instant or time period!");
+			}
+		}
+		else {
+			throw new Exception("PHENTIME property needs to be specified in properties file!!");
+		}
+		
+	}
+}
