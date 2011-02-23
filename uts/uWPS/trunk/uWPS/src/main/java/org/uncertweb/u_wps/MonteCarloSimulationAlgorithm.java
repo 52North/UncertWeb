a612da91f54ed89bbcd613b1d5839395066eb70e
Benjamin Pross <b.pross@52north.org>
2011-02-23 13:37:35 +0000

    Initial import.

---
+++ uts/uWPS/trunk/uWPS/src/main/java/org/uncertweb/u_wps/MonteCarloSimulationAlgorithm.java
@@ -0,0 +1,36 @@
+package org.uncertweb.u_wps;
+
+import java.util.List;
+import java.util.Map;
+
+import org.n52.wps.server.AbstractAlgorithm;
+
+
+/**
+ * process implements algorithm for executing monteCarlo simulation
+ * 
+ * @author staschc
+ *
+ */
+public class MonteCarloSimulationAlgorithm extends AbstractAlgorithm{
+
+	public List getErrors() {
+		return null;
+	}
+
+	public Class getInputDataType(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Class getOutputDataType(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Map run(Map inputData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
