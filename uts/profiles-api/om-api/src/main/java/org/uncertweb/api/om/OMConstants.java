65df11e8f586e932e1c400200e67b1b5b581e49a
Christoph Stasch <c.stasch@52north.org>
2011-03-14 14:08:57 +0000

    added documentation, removed unused constants

--- uts/profiles-api/om-api/src/main/java/org/uncertweb/api/om/OMConstants.java
+++ uts/profiles-api/om-api/src/main/java/org/uncertweb/api/om/OMConstants.java
@@ -2,53 +2,38 @@ package org.uncertweb.api.om;
 
 /**
  * contains constants like namespace urls
- * @author Kiesow
+ * 
+ * @author Kiesow, staschc
  *
  */
 public class OMConstants {
 
 	// namespaces and prefixes
-	
+	/**namespace URL for Observations and Measurements*/
 	public static final String NS_OM = "http://www.opengis.net/om/2.0";
+	/**namespace prefix for Observations and Measurements*/
 	public static final String NS_OM_PREFIX = "om";
 	
+	/**namespace URL for spatial sampling features*/
 	public static final String NS_SAMS = "http://www.opengis.net/samplingSpatial/2.0";
+	/**namespace prefix for spatial sampling features*/
 	public static final String NS_SAMS_PREFIX = "sams";
+	
+	/**namespace URL for sampling features*/
 	public static final String NS_SA = "http://www.opengis.net/sampling/2.0";
+	/**namespace prefix for sampling features*/
 	public static final String NS_SA_PREFIX = "sa";
 	
+	/**namespace URL for xlinks*/
 	public static final String NS_XLINK = "http://www.w3.org/1999/xlink";
+	/**namespace prefix for xlinks*/
 	public static final String NS_XLINK_PREFIX = "xlink";
 	
+	/**namespace URL for GML 3.2*/
 	public static final String NS_GML = "http://www.opengis.net/gml/3.2";
+	/**namespace prefix for GML 3.2*/
 	public static final String NS_GML_PREFIX = "gml";
 	
 	
-	// attribute names
-
-    public static final String AN_ID = "id";
-    
-    public static final String AN_HREF = "href";
 	
-    
-	// names of elements in O&M documents
-	
-    public static final String EN_OBSERVED_PROPERTY = "observedProperty";
-
-    public static final String EN_OBSERVATION = "Observation";
-
-    public static final String EN_PHENOMENON = "Phenomenon";
-
-    public static final String EN_RESULT = "result";
-
-    public static final String EN_TIME_PERIOD = "TimePeriod";
-
-    public static final String EN_TIME_INSTANT = "TimeInstant";
-    
-    public static final String EN_PHENOMENON_TIME = "phenomenonTime";
-    
-    public static final String EN_RESULT_TIME = "resultTime";
-    
-    public static final String EN_VALID_TIME = "validTime";
-    
 }
