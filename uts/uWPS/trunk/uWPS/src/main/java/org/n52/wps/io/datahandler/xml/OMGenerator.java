3fd226fd086a1769a98dce52cc483085ee9cb51f
Christoph Stasch <c.stasch@52north.org>
2011-03-15 12:36:58 +0000

    classes for encoding and parsing of O&M

---
+++ uts/uWPS/trunk/uWPS/src/main/java/org/n52/wps/io/datahandler/xml/OMGenerator.java
@@ -0,0 +1,28 @@
+package org.n52.wps.io.datahandler.xml;
+
+import java.io.OutputStream;
+
+import org.n52.wps.io.data.IData;
+import org.w3c.dom.Node;
+
+public class OMGenerator extends AbstractXMLGenerator{
+
+	@Override
+	public Node generateXML(IData arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OutputStream generate(IData arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class[] getSupportedInternalInputDataType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
