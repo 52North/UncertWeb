0e87f692ebe2ce207c00093c96426fa424b4fc4c
Christoph Stasch <c.stasch@52north.org>
2011-04-05 14:51:10 +0000

    simplified: removed gmlID and ValueUnit class

--- uts/profiles-api/om-api/src/main/java/org/uncertweb/api/om/DQ_UncertaintyResult.java
+++ uts/profiles-api/om-api/src/main/java/org/uncertweb/api/om/DQ_UncertaintyResult.java
@@ -10,17 +10,12 @@ import org.uncertml.IUncertainty;
  */
 public class DQ_UncertaintyResult {
 
-	/**gml id*/
-	private String id;
-	
-	/**uu id inherited from DQ_AbstractResult*/
-	private String uuid;
 	
 	/**array containing uncertainties*/
 	private IUncertainty[] values;
 	
 	/**definition of value unit*/
-	private UnitDefinition valueUnit;
+	private String uom;
 	
 	/**
 	 * Constructor
@@ -30,66 +25,17 @@ public class DQ_UncertaintyResult {
 	 * @param valueUnit 
 	 * 			the value's unit
 	 */
-	public DQ_UncertaintyResult(IUncertainty[] values, UnitDefinition valueUnit) {
+	public DQ_UncertaintyResult(IUncertainty[] values, String uom) {
 		
 		this.setValues(values);
-		this.valueUnit = valueUnit;
+		this.uom = uom;
 	}
 	
-	/**
-	 * Constructor
-	 * 
-	 * @param id (optional)
-	 * 			gml id
-	 * @param uuid (optional)
-	 * 			uu id inherited from DQ_AbstractResult
-	 * @param values 
-	 * 			values of uncertainties
-	 * @param valueUnit 
-	 * 			the value's uni
-	 */
-	public DQ_UncertaintyResult(String id, String uuid, IUncertainty[] value, UnitDefinition valueUnit) {
-		this(value, valueUnit);
-		
-		this.setId(id);
-		this.uuid = uuid;
-	}
 
 
 	// getters and setters
-	/**
-	 * 
-	 * @return Returns GML ID
-	 */
-	public String getId() {
-		return id;
-	}
 	
-	/**
-	 * 
-	 * @param id
-	 * 		GML ID
-	 */
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	/**
-	 * 
-	 * @return Returns UU ID inherited from DQ_AbstractResult
-	 */
-	public String getUuid() {
-		return uuid;
-	}
 
-	/**
-	 * 
-	 * @param uuid
-	 * 			UU ID inherited from DQ_AbstractResult
-	 */
-	public void setUuid(String uuid) {
-		this.uuid = uuid;
-	}
 
 	/**
 	 * 
@@ -108,22 +54,24 @@ public class DQ_UncertaintyResult {
 	public void setValues(IUncertainty[] values) {
 		this.values = values;
 	}
-	
+
+
+
 	/**
-	 * 
-	 * @return Returns the value's unit
+	 * @return the uom
 	 */
-	public UnitDefinition getValueUnit() {
-		return valueUnit;
+	public String getUom() {
+		return uom;
+	}
+
+
+
+	/**
+	 * @param uom the uom to set
+	 */
+	public void setUom(String uom) {
+		this.uom = uom;
 	}
 	
-	/**
-	 * 
-	 * @param valueUnit
-	 * 			the value's unit
-	 */
-	public void setValueUnit(UnitDefinition valueUnit) {
-		this.valueUnit = valueUnit;
-	}
 
 }
