31cf00d7fb9c2425e3e0ebaf596f5f48ff81e1a9
Christoph Stasch <c.stasch@52north.org>
2011-04-05 14:49:21 +0000

    added geometry types, removed unused imports

--- uts/profiles-api/gml-api/src/main/java/org/uncertweb/api/gml/UwGMLUtil.java
+++ uts/profiles-api/gml-api/src/main/java/org/uncertweb/api/gml/UwGMLUtil.java
@@ -1,9 +1,5 @@
 package org.uncertweb.api.gml;
 
-import java.util.Hashtable;
-import java.util.Map;
-
-import org.apache.xmlbeans.XmlOptions;
 import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
 
@@ -19,6 +15,18 @@ public class UwGMLUtil {
 	//CONSTANTS
 	/**constant for EPSG URL used in srsName*/
 	public final static String EPSG_URL = "http://www.opengis.net/def/crs/EPSG/0/";
+	
+	///Constants for Geometry type names
+	public final static String POINT_TYPE="Point";
+	public final static String LINESTRING_TYPE="LineString";
+	public final static String POLYGON_TYPE="Polygon";
+	public final static String RECTIFIEDGRID_TYPE="RectifiedGrid";
+	public final static String MULTIPOINT_TYPE="MultiPoint";
+	public final static String MULTILINESTRING_TYPE="MultiLineString";
+	public final static String MULTIPOLYGON_TYPE="MultiPolygon";
+	public final static String MULTIRECTIFIEDGRID_TYPE="MultiRectifiedGrid";
+	
+	
 	
 	///////////////////////////////////////////////////////////////////
 	// HELPER METHODS
