a24cee839fba437a712164b257c06c749b206d8c
Christoph Stasch <c.stasch@52north.org>
2011-10-10 14:49:38 +0000

    initial import

---
+++ albatross-wps/src/main/java/org/uncertweb/wps/AlbatrossProcess.java
@@ -0,0 +1,43 @@
+package org.uncertweb.wps;
+
+import java.util.List;
+import java.util.Map;
+
+import org.n52.wps.io.data.IData;
+import org.n52.wps.server.AbstractAlgorithm;
+
+
+/**
+ * implements a process that invokes the Albatross Model and returns the outputs according to the
+ * UncertWeb profiles
+ * 
+ * @author Steffan Voss
+ *
+ */
+public class AlbatrossProcess extends AbstractAlgorithm{
+
+	@Override
+	public List<String> getErrors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class getInputDataType(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class getOutputDataType(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, IData> run(Map<String, List<IData>> inputData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
