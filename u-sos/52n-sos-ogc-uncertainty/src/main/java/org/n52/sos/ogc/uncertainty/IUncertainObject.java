603f463714ec86f5a87a051c2193ca9c4ee06481
Martin Kiesow <martin.kiesow@uni-muenster.de>
2011-10-10 15:33:46 +0000

    uncertain observations added

---
+++ u-sos/52n-sos-ogc-uncertainty/src/main/java/org/n52/sos/ogc/uncertainty/IUncertainObject.java
@@ -0,0 +1,13 @@
+package org.n52.sos.ogc.uncertainty;
+
+/**
+ * Interface to handle various uncertain objects as Observations
+ * 
+ * @author Kiesow
+ *
+ */
+public interface IUncertainObject {
+	
+	//TODO find fitting return type
+	public String getUncertainty();
+}
