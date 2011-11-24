ac439a814041a7b3d7def632c6b8f836db5fc401
Christian Autermann <c.autermann@52north.org>
2011-11-24 16:20:48 +0000

    some logging

--- 52n-wps-io-uncertweb/src/main/java/org/n52/wps/io/datahandler/DelegatingParser.java
+++ 52n-wps-io-uncertweb/src/main/java/org/n52/wps/io/datahandler/DelegatingParser.java
@@ -1,5 +1,7 @@
 package org.n52.wps.io.datahandler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.uncertweb.utils.UwCollectionUtils;
 
 import java.io.InputStream;
@@ -9,6 +11,8 @@ import org.n52.wps.io.IParser;
 import org.n52.wps.io.data.IData;
 
 public abstract class DelegatingParser extends DelegatingHandler implements IParser {
+	
+	private static final Logger log = LoggerFactory.getLogger(DelegatingParser.class);
 
 	private final Set<IParser> parsers;
 	
@@ -29,7 +33,9 @@ public abstract class DelegatingParser extends DelegatingHandler implements IPar
 	}
 	
 	private IParser findParser(String mimeType, String schema) {
+		log.info("Searching for Parser. Mime-Type: {}, Schema: {}", mimeType, schema);
 		for (IParser g : parsers) {
+			log.info("Asking Parser: {}", g.getClass().getName());
 			if (g.isSupportedFormat(mimeType)
 			 && g.isSupportedSchema(schema)) {
 				return g;
