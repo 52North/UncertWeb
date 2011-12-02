a8179a9c1a34eeff559e32e86f02f79761efe817
Christoph Stasch <c.stasch@52north.org>
2011-12-02 14:25:25 +0000

    dummy test added

--- uncertml-x-api/src/test/java/org/uncertml/io/test/ExtendedXMLEncoderTest.java
+++ uncertml-x-api/src/test/java/org/uncertml/io/test/ExtendedXMLEncoderTest.java
@@ -1,7 +1,12 @@
 package org.uncertml.io.test;
 
+
 import junit.framework.TestCase;
 
 public class ExtendedXMLEncoderTest extends TestCase {
+	
+	public void testEncoder() throws Exception{
+		System.out.println("needs to be done!");
+	}
 
 }
