35d0fea8382d73f7699f6bbe659e3f5cc7a77cf3
Christian Autermann <c.autermann@52north.org>
2012-08-03 09:35:09 +0000

    gridCooridinate encoder hook missing license headers

--- viss/src/main/java/org/uncertweb/viss/core/web/provider/UncertaintyObservationCollectionProvider.java
+++ viss/src/main/java/org/uncertweb/viss/core/web/provider/UncertaintyObservationCollectionProvider.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2011 52° North Initiative for Geospatial Open Source Software
+ *                   GmbH, Contact: Andreas Wytzisk, Martin-Luther-King-Weg 24,
+ *                   48155 Muenster, Germany                  info@52north.org
+ *
+ * Author: Christian Autermann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,51 Franklin
+ * Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.uncertweb.viss.core.web.provider;
 
 import java.io.IOException;
@@ -12,9 +33,16 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.uncertweb.api.om.exceptions.OMEncodingException;
+import org.uncertweb.api.om.io.AbstractHookedObservationEncoder.EncoderHook;
 import org.uncertweb.api.om.io.JSONObservationEncoder;
+import org.uncertweb.api.om.observation.AbstractObservation;
 import org.uncertweb.api.om.observation.collections.IObservationCollection;
+import org.uncertweb.netcdf.NcUwObservation;
+import org.uncertweb.utils.UwCollectionUtils;
 import org.uncertweb.viss.core.VissError;
 import org.uncertweb.viss.core.util.MediaTypes;
 
@@ -23,6 +51,27 @@ import org.uncertweb.viss.core.util.MediaTypes;
 public class UncertaintyObservationCollectionProvider implements
 		MessageBodyWriter<IObservationCollection> {
 
+	
+	
+	@SuppressWarnings("unchecked")
+	private JSONObservationEncoder enc = new JSONObservationEncoder(
+			UwCollectionUtils.<EncoderHook<JSONObject>>collection(new EncoderHook<JSONObject>() {
+		@Override
+		public void encode(AbstractObservation ao, JSONObject encodedObject) throws OMEncodingException {
+			if (ao instanceof NcUwObservation) {
+				try {
+					JSONArray coordinates = new JSONArray();
+					for (int i : ((NcUwObservation) ao).getGridCoordinates().getCoordinateValues()) {
+						coordinates.put(i);
+					}
+					encodedObject.put("gridCoordinate", coordinates);
+				} catch (JSONException e) {
+					throw new OMEncodingException(e);
+				}
+			}
+		}
+	}));
+	
 	@Override
 	public boolean isWriteable(Class<?> t, Type gt, Annotation[] a, MediaType mt) {
 		return IObservationCollection.class.isAssignableFrom(t)
@@ -40,7 +89,7 @@ public class UncertaintyObservationCollectionProvider implements
 			Annotation[] a, MediaType mt, MultivaluedMap<String, Object> h,
 			OutputStream out) throws IOException, WebApplicationException {
 		try {
-			new JSONObservationEncoder().encodeObservationCollection(t, out);
+			enc.encodeObservationCollection(t, out);
 		} catch (OMEncodingException e) {
 			throw VissError.internal(e);
 		}
