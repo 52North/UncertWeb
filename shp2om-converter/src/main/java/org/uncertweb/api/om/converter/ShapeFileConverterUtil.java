afc88216b41d14e88242a4241d3fc8214ec7a9c4
Christoph Stasch <c.stasch@52north.org>
2012-08-07 14:22:44 +0000

    revision of imports, static helper method for creating sampling features added

--- shp2om-converter/src/main/java/org/uncertweb/api/om/converter/ShapeFileConverterUtil.java
+++ shp2om-converter/src/main/java/org/uncertweb/api/om/converter/ShapeFileConverterUtil.java
@@ -1,8 +1,19 @@
 package org.uncertweb.api.om.converter;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import org.uncertweb.api.gml.Identifier;
 import org.uncertweb.api.om.TimeObject;
+import org.uncertweb.api.om.sampling.SpatialSamplingFeature;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
 
 /**
  * Util class which contains some helper methods which are used in several classes
@@ -11,6 +22,7 @@ import org.uncertweb.api.om.TimeObject;
  *
  */
 public class ShapeFileConverterUtil {
+
 	
 	/**
 	 * parses a timestring which can contain either just one single ISO 8601 String or two comma-seperated time strings
@@ -43,5 +55,59 @@ public class ShapeFileConverterUtil {
 			throw new IOException("PHENTIME property needs to be specified in properties file!!");
 		}
 		
+	}
+	
+	/**
+	 * helper method for creating SpatialSamplingFeature from featureID and JTS
+	 * geometry
+	 * 
+	 * @param id
+	 *            gml id of the geometry
+	 * @param geom
+	 *            JTS geometry of the sampling feature
+	 * @return POJO representation of the SamplingFeature
+	 * @throws URISyntaxException 
+	 *             if parsing fails
+	 */
+	public static SpatialSamplingFeature createSamplingFeature(String id,
+			Geometry geom) throws URISyntaxException {
+		SpatialSamplingFeature sf = null;
+		int srid = geom.getSRID();
+		int multiGeomCounter=0;
+		if (geom instanceof MultiLineString) {
+			MultiLineString mls = (MultiLineString) geom;
+			int size = mls.getNumGeometries();
+			LineString[] lsArray = new LineString[size];
+			for (int i = 0; i < size; i++) {
+				lsArray[i] = new GeometryFactory().createLineString(((LineString) mls
+						.getGeometryN(i)).getCoordinateSequence());
+				lsArray[i].setSRID(srid);
+				multiGeomCounter++;
+			}
+			MultiLineString gmlLineString =  new GeometryFactory().createMultiLineString(lsArray);
+			Identifier identifier = new Identifier(new URI("http://www.uncertweb.org"),id);
+			sf = new SpatialSamplingFeature(identifier,null, gmlLineString);
+		}
+		else if (geom instanceof MultiPolygon) {
+//			MultiPolygon mp = (MultiPolygon) geom;
+//			int size = mp.getNumGeometries();
+//			LineString[] lsArray = new LineString[size];
+//			for (int i = 0; i < size; i++) {
+//				lsArray[i] = new GeometryFactory().createLineString(((MultiPolygon) mls
+//						.getGeometryN(i)).getCoordinateSequence());
+//				lsArray[i].setSRID(srid);
+//				multiGeomCounter++;
+//			}
+//			MultiPolygon
+//			MultiLineString gmlLineString =  new GeometryFactory().createMultiLineString(lsArray);
+			Identifier identifier = new Identifier(new URI("http://www.uncertweb.org"),id);
+			sf = new SpatialSamplingFeature(identifier,null, geom);
+		}
+		else if (geom instanceof Point) {
+			Identifier identifier = new Identifier(new URI("http://www.uncertweb.org"),id);
+			sf = new SpatialSamplingFeature(identifier,null, geom);
+		}
+		// TODO add further geometry types	
+		return sf;
 	}
 }
