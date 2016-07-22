a0f89612a8e3f5b2f4d0df9e8b9251e4e0d75383
Christian Autermann <c.autermann@52north.org>
2016-07-22 09:19:11 +0200

    adjusted pom.xml

--- stas/src/main/java/org/n52/wps/io/datahandler/generator/ObservationCollectionGenerator.java
+++ stas/src/main/java/org/n52/wps/io/datahandler/generator/ObservationCollectionGenerator.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2011 52° North Initiative for Geospatial Open Source Software 
- *                   GmbH, Contact: Andreas Wytzisk, Martin-Luther-King-Weg 24, 
+ * Copyright (C) 2011 52° North Initiative for Geospatial Open Source Software
+ *                   GmbH, Contact: Andreas Wytzisk, Martin-Luther-King-Weg 24,
  *                   48155 Muenster, Germany                  info@52north.org
  *
  * Author: Christian Autermann
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later 
+ * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT 
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc.,51 Franklin
  * Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -37,16 +37,17 @@ import net.opengis.om.x10.ObservationCollectionDocument;
 import net.opengis.om.x10.ObservationCollectionType;
 import net.opengis.om.x10.ObservationDocument;
 
-import org.n52.wps.io.data.IData;
-import org.n52.wps.io.data.binding.complex.OMBinding;
-import org.n52.wps.io.datahandler.AbstractUwGenerator;
 import org.uncertweb.api.om.observation.AbstractObservation;
 import org.uncertweb.utils.UwCollectionUtils;
 import org.uncertweb.utils.UwXmlUtils;
 
+import org.n52.wps.io.data.IData;
+import org.n52.wps.io.data.binding.complex.OMBinding;
+import org.n52.wps.io.datahandler.AbstractUwGenerator;
+
 /**
  * Generator for {@link ObservationCollectionDocument}s.
- * 
+ *
  * @author Christian Autermann <autermann@uni-muenster.de>
  */
 public class ObservationCollectionGenerator extends AbstractUwGenerator {
--- stas/src/test/java/org/uncertweb/sta/utils/FeatureCacheTest.java
+++ stas/src/test/java/org/uncertweb/sta/utils/FeatureCacheTest.java
@@ -1,35 +1,23 @@
 package org.uncertweb.sta.utils;
 
-import static org.junit.Assert.*;
-
 import java.net.MalformedURLException;
-import java.net.URL;
 
-import org.joda.time.Period;
 import org.junit.Test;
 import org.uncertweb.sta.wps.STASException;
-import org.uncertweb.utils.UwTimeUtils;
 
 public class FeatureCacheTest {
-	static String TEST_URL = "http://uncertdata.aston.ac.uk:8080/geoserver/UncertWeb/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=UncertWeb:feature_of_interest&featureID=FERA_Anglia_fields_TL17378558";
-	
-	@Test
-	public void testGetGeometryFromWfs() {
-		try {
-			URL url = new URL(TEST_URL);
-			FeatureCache fc = new FeatureCache();
-			fc.getFeatureFromWfs(url);
-			URL url2= new URL("http://uncertdata.aston.ac.uk:8080/geoserver/UncertWeb/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=UncertWeb:feature_of_interest&featureID=FERA_Anglia_fields_TL72664141");
-			fc.getFeatureFromWfs(url2);
-			fc.getFeatureFromWfs(url);
-			
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (STASException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    private static final String URL1
+            = "http://uncertdata.aston.ac.uk:8080/geoserver/UncertWeb/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=UncertWeb:feature_of_interest&featureID=FERA_Anglia_fields_TL17378558";
+    private static final String URL2
+            = "http://uncertdata.aston.ac.uk:8080/geoserver/UncertWeb/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=UncertWeb:feature_of_interest&featureID=FERA_Anglia_fields_TL72664141";
+
+    @Test
+    public void testGetGeometryFromWfs()
+            throws STASException, MalformedURLException {
+        FeatureCache fc = new FeatureCache();
+        fc.getFeatureFromWfs(URL1);
+        fc.getFeatureFromWfs(URL2);
+        fc.getFeatureFromWfs(URL1);
+    }
 
 }
